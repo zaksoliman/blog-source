a28788fcf164297f04cc88101c39f28076917155
Zack Soliman <zack.soliman@gmail.com>
2018-04-20 22:05:03 -0400

    Styling

---
+++ content/introductions.md
@@ -0,0 +1,5 @@
+Title: Introductions
+Date: 2014-12-13 18:32
+Category: Test
+
+Hello world from Pelican!
--- pelicanconf.py
+++ pelicanconf.py
@@ -12,7 +12,13 @@ THEME = 'themes/voce'
 PLUGIN_PATHS = os.path.join(THEME, "plugins")
 TIMEZONE = 'America/Montreal'
 
+TAGS_URL = 'tags.html'
+ARCHIVES_URL = 'archives.html'
+
 DEFAULT_LANG = 'en'
+FUZZY_DATES = True
+
+PLUGINS = ["assets"]
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -31,7 +37,24 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
+MANGLE_EMAILS = True
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+GOOGLE_ANALYTICS_ID = None
+GOOGLE_ANALYTICS_PROP = None
+
+GLOBAL_KEYWORDS = ["data science",
+                   "machine learning",
+                   "deep learning",
+                   "programming",
+                   "coding",
+                   "python",
+                   "lisp",
+                   "clojure",
+                   "hacking",
+                   "computer science",
+                   "math",
+                   "mathematics"]
