551acd08c63b312f405c42e0dcac9be47cc720b7
Zack Soliman <zack.soliman@gmail.com>
2019-03-07 16:36:13 -0500

    Sync

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-import os
 
-AUTHOR = 'Zack Soliman'
+AUTHOR = 'Zak Soliman'
 SITENAME = 'Exponential Divergence'
 SITEURL = ''
 
 PATH = 'content'
-THEME = 'themes/voce'
-PLUGIN_PATHS = os.path.join(THEME, "plugins")
-TIMEZONE = 'America/Montreal'
 
-TAGS_URL = 'tags.html'
-ARCHIVES_URL = 'archives.html'
+TIMEZONE = 'America/Toronto'
 
 DEFAULT_LANG = 'en'
-FUZZY_DATES = True
-
-PLUGINS = ["assets"]
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -28,33 +20,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-#LINKS = (('Pelican', 'http://getpelican.com/'),
-#         ('Python.org', 'http://python.org/'),
-#         ('Jinja2', 'http://jinja.pocoo.org/'),
-#         ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-#SOCIAL = (('You can add links in your config file', '#'),
-#          ('Another social link', '#'),)
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
-MANGLE_EMAILS = True
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
-GOOGLE_ANALYTICS_ID = None
-GOOGLE_ANALYTICS_PROP = None
-
-GLOBAL_KEYWORDS = ["data science",
-                   "machine learning",
-                   "deep learning",
-                   "programming",
-                   "coding",
-                   "python",
-                   "lisp",
-                   "clojure",
-                   "hacking",
-                   "computer science",
-                   "math",
-                   "mathematics"]
